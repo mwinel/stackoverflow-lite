73a3b60cf630d01187ba1981f122ece1ccb5c61b
Nelson Murungi <nelsonmwirumubi@gmail.com>
2018-08-17 20:25:06 +0300

    Add some mock questions

--- app/tests/base.py
+++ app/tests/base.py
@@ -11,3 +11,14 @@ class BaseTestCase(TestCase):
     def setUp(self):
         """ Create test database and set up test client. """
         self.app = app.test_client()
+        # Add some questions
+        self.question = {
+            "title": "Something went wrong with PyQt5 menu: GUI",
+            "body": "There was something that wrong when I was using \
+                    PyQt5 to build a GUI window with a menu bar."
+        }
+        self.question2 = {
+            "title": "ImportError: Can not import app from name 'app'",
+            "body": "I found this error when I tried to execute my code \
+                    from the flask-mega-tutorial."
+        }
