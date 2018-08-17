3c35e01d95c67a4d33c2a44f03423bfdb1611984
Nelson Murungi <nelsonmwirumubi@gmail.com>
2018-08-17 20:33:57 +0300

    [#159767264] Add resource to add question

---
+++ app/main/questions/resources.py
@@ -0,0 +1,35 @@
+from flask import jsonify, request
+from app.main.questions import blueprint
+from app.models import Question
+
+# Initialize a questions list
+questions = []
+
+@blueprint.route('/questions', methods = ['POST'])
+def add_question():
+    data = request.get_json()
+    title = data['title']
+    body = data['body']
+    if title.strip() == "":
+        return jsonify({'error': 'Title is missing'}), 400
+    if len(title.strip()) < 15:
+        return jsonify({'error': 'Title must be atleast 15 characters'}), 400
+    if body.strip() == "":
+        return jsonify({'error': 'Body is missing'}), 400
+    if len(body.strip()) < 30:
+        return jsonify({'error': 'Body must be atleast 15 characters'}), 400
+    global id
+    if len(questions) == 0:
+        id = len(questions) + 1
+    else:
+        id = id + 1
+    question = {
+        'id': id,
+        'title': request.json['title'],
+        'body': request.json['body']
+    }
+    questions.append(question)
+    return jsonify({
+        'message': 'question successfully created'
+    }), 201
+
