3129689d5099c16baeb8ac6ac2f5296fea80065d
Nelson Murungi <nelsonmwirumubi@gmail.com>
2018-08-17 21:11:59 +0300

    [#159761345] Add tests for fetch questions resource

--- app/tests/test_questions.py
+++ app/tests/test_questions.py
@@ -6,12 +6,22 @@ class TestQuestionCase(BaseTestCase):
     """ Test index endpoints """
 
     def test_create_question(self):
-        """ Test API can create a question. """
+        """ Test API can create a question """
         rv = self.app.post('/api/v1/questions', data=json.dumps(self.question), 
             content_type='application/json')
         self.assertEqual(rv.status_code, 201)
         output = json.loads(rv.data)
         self.assertTrue("question successfully created" in output["message"])
+
+    def test_fetch_questions(self):
+        """ Test API can fetch all questions """
+        rv = self.app.post('/api/v1/questions', data=json.dumps(self.question), 
+            content_type='application/json')
+        self.assertEqual(rv.status_code, 201)
+        rv = self.app.get('/api/v1/questions', data=json.dumps(self.question),
+            content_type='application/json')
+        self.assertEqual(rv.status_code, 200)
+        self.assertIn("Something went wrong with PyQt5 menu: GUI", str(rv.data))
 
 if __name__ == '__main__':
     unittest.main()
