33dcfbae6b05d13601ec9d940f88b7972d1c3aa2
Nelson Murungi <nelsonmwirumubi@gmail.com>
2018-08-17 20:28:24 +0300

    [#159767264] Define question class

---
+++ app/models.py
@@ -0,0 +1,8 @@
+# Define a class Question
+class Question:
+    
+    # Constructor to create class objects
+    def __init__(self, id, title, body):
+        self.id = id
+        self.title = title
+        self.body = body
