6ae9f3f88cf355f3fda00539394f2a29509ba30e
Nelson Murungi <nelsonmwirumubi@gmail.com>
2018-08-19 13:35:09 +0300

    [#159761345] Add method to get all questions

--- app/main/questions/resources.py
+++ app/main/questions/resources.py
@@ -3,8 +3,6 @@ from flask import jsonify, request, make_response
 from app.main.questions import blueprint
 from app.models import Question
 
-# Initialize a questions list
-questions = []
 
 @blueprint.route('/questions', methods = ['POST'])
 def add_question():
@@ -19,22 +17,14 @@ def add_question():
         return jsonify({'error': 'Body is missing'}), 400
     if len(body.strip()) < 30:
         return jsonify({'error': 'Body must be atleast 15 characters'}), 400
-    global id
-    if len(questions) == 0:
-        id = len(questions) + 1
-    else:
-        id = id + 1
-    question = {
-        'id': id,
-        'title': request.json['title'],
-        'body': request.json['body']
-    }
-    questions.append(question)
+    Question.add()
     return jsonify({
         'message': 'question successfully created'
     }), 201
 
+
 @blueprint.route('/questions', methods = ['GET'])
 def fetch_questions():
-    return make_response(json.dumps({'questions': questions})), 200, \
+    all_questions = Question.get_all()
+    return make_response(json.dumps({'questions': all_questions})), 200, \
         {'Content-Type': 'application/json'}
