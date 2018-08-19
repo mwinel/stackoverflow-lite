0658a958a8c0a940ac72a3ed45e637e656a97658
Nelson Murungi <nelsonmwirumubi@gmail.com>
2018-08-19 13:35:54 +0300

    [#159761345] Refactor to implement get all method

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
+from flask import jsonify, request, abort
+
+# Initialize a questions list
+questions = []
+
 # Define a class Question
 class Question:
     
@@ -6,3 +11,21 @@ class Question:
         self.id = id
         self.title = title
         self.body = body
+
+    @staticmethod
+    def add():
+        global id
+        if len(questions) == 0:
+            id = len(questions) + 1
+        else:
+            id = id + 1
+        question = {
+            'id': id,
+            'title': request.json['title'],
+            'body': request.json['body']
+        }
+        questions.append(question)
+
+    @staticmethod
+    def get_all():
+        return questions
