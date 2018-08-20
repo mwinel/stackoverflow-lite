da8ca031fe650d60dd70cb292e974fbd4ff310c2
Nelson Murungi <mwinel@live.com>
2018-08-21 01:41:14 +0300

    Revert "Development Branch"

--- app/main/answers/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-blueprint = Blueprint('answers', __name__)
-
-from app.main.answers import resources
--- app/main/questions/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-blueprint = Blueprint('questions', __name__)
-
-from app.main.questions import resources
