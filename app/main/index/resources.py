24e025e2948640aefeca2f2310a59db16a1c6169
Nelson Murungi <nelsonmwirumubi@gmail.com>
2018-08-21 02:12:23 +0300

    Remove duplicate commits

--- app/main/index/resources.py
+++
@@ -1,7 +0,0 @@
-from flask import jsonify
-from app.main.index import blueprint
-
-@blueprint.route('/')
-@blueprint.route('/index')
-def index():
-    return jsonify({'message': 'Welcome to StackOverflow-Lite'}), 200
